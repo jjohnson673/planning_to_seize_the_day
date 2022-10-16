e28da06cbf8afebf5ab9e62bdfab6e0f634ecf15
jjohnson673 <jkjohnson673@gmail.com>
2022-10-16 14:30:21 -0400

    eventually found command that was snagging files

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 
  // display current day on page
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+// $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+
+var currentDay = moment().format("dddd, MMM Do YYYY");
+var currentTime = moment().hour();
+$("#currentDay").html(currentDay);
 
 
 
-$(document).ready(function(){
+
+$(document).ready(function() {
                 
            // listen for save button clicks
-           $('.saveBtn').on('click', function () {       
+           $(".saveBtn").on("click", function () {       
                // get nearby values in JQuery
                var text = $(this).siblings(".description").val();
                var time = $(this).parent().attr("id");
@@ -18,32 +23,32 @@ $(document).ready(function(){
                localStorage.setItem(time, text);
       
        
-               // Show notification that item was saved to localStorage by adding class 'show'
+               // Show notification that item was saved to localStorage by adding class "show"
        
-               // Timeout to remove 'show' class after 5 seconds
+               // Timeout to remove "show" class after 5 seconds
             //    setTimeout(function () {
-            //        $('.notification').removeClass('show');
+            //        $(".notification").removeClass("show");
             //    }, 5000);
            })
        
-       function hourUpdater() {
+    //    function hourUpdater() {
                // get current number of hours (preferably with moment.js)
-               var currentTime = moment().hour();
+            //    var currentTime = moment().hour();
        
                // loop over time blocks ---> https://api.jquery.com/each/
-               // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
+               // inside this loop, // check if we"ve moved past this time. If we have, make the row grey. If it"s future, make it green. if it"s past, make it red. Using the past, present, and future classes in css file
 
 
                $(".time-block").each(function () {
                 var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-                console.log(blockTime, timeNow);
+                console.log(blockTime, currentTime);
 
-                if (blockTime < timeNow) {
+                if (blockTime < currentTime) {
                     $(this).removeClass("future");
                     $(this).removeClass("present");
                     $(this).addClass("past");
                 }
-                else if (blockTime === timeNow) {
+                else if (blockTime === currentTime) {
                     $(this).removeClass("past");
                     $(this).removeClass("future");
                     $(this).addClass("present");
@@ -57,23 +62,23 @@ $(document).ready(function(){
 
                    
        
-           hourUpdater();
+        //    hourUpdater();
        
-           // set up interval to check if current time needs to be updated
-           var interval = setInterval(hourUpdater, 15000);
+        //    // set up interval to check if current time needs to be updated
+        //    var interval = setInterval(hourUpdater, 15000);
        
            // load any saved data from localStorage
-           $('#hour9 .description').val(localStorage.getItem('hour9'));
-           $('#hour10 .description').val(localStorage.getItem('hour10'));
-           $('#hour11 .description').val(localStorage.getItem('hour11'));
-           $('#hour12 .description').val(localStorage.getItem('hour12'));
-           $('#hour13 .description').val(localStorage.getItem('hour13'));
-           $('#hour14 .description').val(localStorage.getItem('hour14'));
-           $('#hour15 .description').val(localStorage.getItem('hour15'));
-           $('#hour16 .description').val(localStorage.getItem('hour16'));
-           $('#hour17 .description').val(localStorage.getItem('hour17'));
+           $("#hour9 .description").val(localStorage.getItem("hour9"));
+           $("#hour10 .description").val(localStorage.getItem("hour10"));
+           $("#hour11 .description").val(localStorage.getItem("hour11"));
+           $("#hour12 .description").val(localStorage.getItem("hour12"));
+           $("#hour13 .description").val(localStorage.getItem("hour13"));
+           $("#hour14 .description").val(localStorage.getItem("hour14"));
+           $("#hour15 .description").val(localStorage.getItem("hour15"));
+           $("#hour16 .description").val(localStorage.getItem("hour16"));
+           $("#hour17 .description").val(localStorage.getItem("hour17"));
           
-       };
+ 
        
 
 });
